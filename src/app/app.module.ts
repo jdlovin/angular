9952dd45c25ab67c401d7f1ecad3a5d4dfd2b7e8
jdlovin <josh.lovin@revature.net>
2021-08-26 14:59:15 -0400

    filling in space

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test.component';
-import { HomeComponent } from './home/home.component';
+
+import { NavBarComponent } from './component/nav-bar/nav-bar.component';
+import { SidebarComponent } from './component/sidebar/sidebar.component';
+import { MainComponent } from './component/main/main.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
-    HomeComponent
+    NavBarComponent,
+    SidebarComponent,
+    MainComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
