7ddef7c30ae1cbf94c7187f0d92a2a5d384bdc08
jdlovin <josh.lovin@revature.net>
2021-08-27 10:56:12 -0400

    interpolation

--- src/app/component/main/main.component.ts
+++ src/app/component/main/main.component.ts
@@ -2,10 +2,25 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main',
-  templateUrl: './main.component.html',
+  template: `
+  
+  <h1>This is just a test</h1>
+  <h1> {{firstName}} </h1>
+  <h1> {{'Welcome ' + firstName}}</h1>
+  
+  
+  
+  `,
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+
+  public firstName = "Jamarcus";
+  public message = "Hi! I'm Paul";
+
+  greetUser(){
+    return 'Hi! ' + this.firstName;
+  }
 
   constructor() { }
 
